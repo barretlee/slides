0e7bd78066b86a7610340ea794e3ebd31dfafa46
阎王 <yanwang.lj@alibaba-inc.com>
2016-06-25 00:44:21 +0800

    modify

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
  * @ref: http://johnhax.net/takahashi/index.js
  */
 
+window.onerror = function() {
+  document.write('观赏本 slide 请使用 Chrome 45+ 等支持 ES6 新特性的浏览器')
+}
+
 void function() {
 'use strict' // for Chrome 47-
 
